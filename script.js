986f4b619239e03d30c2255834393b1302f684ed
Sumalatha S <sumalatha15sgowda@gmail.com>
2026-06-17 22:12:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//var employeeName="Sumalatha";
+//console.log(employeeName);
+
+//let employeeAge=22;
+//console.log(employeeAge);
+
+//const companyName="Stackly";
+//console.log(companyName);
+
+//var employeeName="Sumalatha";
+//let employeeAge=22;
+//const companyName="Stackly";
+//console.log(employeeName);
+//console.log(employeeAge);
+//console.log(companyName);
+
+
+//alert("Welcome to Employee Portal")
+
+//let taskCompleted=confirm("Have you completed the today's task?");
+//console.log(taskCompleted);
+
+//let employeeName=prompt("Enter your name");
+//console.log(employeeName);
+
+//let employeeName=prompt("Enter your name");
+//document.writeln("Welcome"+employeeName);
+
+//console.warn("Task submitted successfully");
+
+//console.warn("Task submitted failed");
+
+//let employeeAge=22;
+//console.log("Before Promotion",employeeAge);
+//employeeAge=23;
+//console.log("After Promotion",employeeAge);
+
+
+
+
+
+
+
+
+
